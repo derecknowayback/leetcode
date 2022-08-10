6251fbd0d46e25085a51e1ec30ff035e561533a6
cjp20030116 <2564938992@qq.com>
2022-08-10 23:22:34 +0800

    430. 扁平化多级双向链表 https://leetcode.cn/problems/flatten-a-multilevel-doubly-linked-list/

---
+++ src/DataStructure/LinkedList/rl92/RL92.java
@@ -0,0 +1,19 @@
+package DataStructure.LinkedList.rl92;
+
+public class RL92 {
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        if(head == null || head.next == null || left == right) return head;
+//        int cnt = 0;
+//        ListNode rHead =
+//    }
+}
