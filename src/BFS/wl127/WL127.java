92a4d705cbe2117a65fcf6b8581b05039c5df1c4
Dereck Chen <92903435+derecknowayback@users.noreply.github.com>
2023-03-19 23:52:31 +0800

    2023年3月19日23:52:15

--- src/BFS/wl127/WL127.java
+++ src/BFS/wl127/WL127.java
@@ -1,7 +1,6 @@
 package BFS.wl127;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class WL127 {
     public static void main(String[] args) {
@@ -14,70 +13,64 @@ public class WL127 {
 
 class Solution {
 
-    class TrieNode{
-        TrieNode []child = new TrieNode[26];
-        int isEnd = -1;
-
-        public void insert(String s,int mark){
-            TrieNode ptr = this;
-            for (int i = 0; i < s.length(); i++) {
-                int index = s.charAt(i) - 'a';
-                if(ptr.child[index] == null)
-                    ptr.child[index] = new TrieNode();
-                ptr = ptr.child[index];
-            }
-            ptr.isEnd = mark;
-        }
-
-        public int search(char[] s){
-            TrieNode ptr = this;
-            for (int i = 0; i < s.length; i++) {
-                int index = s[i] - 'a';
-                if(ptr.child[index] == null)
-                    return -1;
-                ptr = ptr.child[index];
-                if(i == s.length - 1) return ptr.isEnd;
-            }
-            return -1;
-        }
-    }
-
-    TrieNode root;
-
-    int res;
+    HashMap<String,Integer> string2id = new HashMap<>();
+    List<List<Integer>> edge = new ArrayList<>();
+    int numString = 0;
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         if(!wordList.contains(endWord)) return 0;
-        root = new TrieNode();
-        res = Integer.MAX_VALUE;
-        for (int i = 0; i < wordList.size(); i++)
-            root.insert(wordList.get(i),i);
-        int[] visit = new int[wordList.size()];
-        Arrays.fill(visit,Integer.MAX_VALUE);
-        bfs(beginWord.toCharArray(),visit,endWord,1);
-        return res == Integer.MAX_VALUE ? 0 : res;
+        // 建图
+        for (String s : wordList) addEdge(s);
+        addEdge(beginWord); // 将出发点加入图
+
+        int[] dis = new int[numString];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        int beginId = string2id.get(beginWord), endId = string2id.get(endWord);
+        dis[beginId] = 0;
+
+        // 开始bfs
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(beginId);
+        while (!queue.isEmpty()){
+            int temp = queue.poll();
+            if(temp == endId){
+                return dis[endId] / 2 + 1; // 除2是因为虚拟节点出去和进来是两次；
+            }
+            for (int k : edge.get(temp)) {
+                // 没有访问过
+                if(dis[k] == Integer.MAX_VALUE){
+                    dis[k] = dis[temp] + 1;
+                    queue.offer(k);
+                }
+            }
+        }
+        return 0;
     }
 
-    public void  bfs(char[] s, int[] visit,String target,int noff){
-        if(target.equals(new String(s))){
-            res = Math.min(noff,res);
-            return;
-        }
-        for (int j = 0; j < s.length; j++) {
-            char origin = s[j];
-            for (int i = 0; i < 26; i++) {
-                char c = (char) ('a' + i);
-                s[j] = c;
-                int search = root.search(s);
-                // 如果 词典有 && 没有使用过
-                if(search != -1 && visit[search] >= noff){
-//                    System.out.println(new String(s));
-                    visit[search] = noff;
-                    bfs(s,visit,target,noff+1);
-//                    visit[search] = false;
-                }// 如果 词典里没有 或者 已经用过了, 恢复并跳过
-            }
-            s[j] = origin;
+    public void addEdge(String s){
+        addWord(s);
+        int id1 = string2id.get(s);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            char origin = chars[i];
+            chars[i] = '*';
+            String k = new String(chars);
+            addWord(k);
+            chars[i] = origin;
+            int id2 = string2id.get(k);
+            edge.get(id1).add(id2);
+            edge.get(id2).add(id1);
         }
     }
+
+    public void addWord(String s){
+        if(!string2id.containsKey(s)){
+            string2id.put(s,numString++);
+            edge.add(new ArrayList<>()); // id和edge中的位置是一一对应的;
+        }
+    }
+
+
+
+
 }
